785a53c10b40cdb809dec180dbaf11440d6441ed
Kumar Panda <niraj.kumar.panda@accenture.com>
2017-04-14 13:39:02 +0530

    fixed thumbnail height mismatch issue

--- seed/product-seeder.js
+++ seed/product-seeder.js
@@ -1,38 +1,39 @@
 var Product = require('../models/product');
 var mongoose = require('mongoose');
+mongoose.Promise = require('bluebird');
 mongoose.connect('localhost:27017/shopping');
 
 var products = [ 
 	new Product({
-	imagePath: 'https://www.macobserver.com/wp-content/uploads/2016/06/google-smartphone.jpg',
-	title: 'Google Pixel',
-	description: 'Very first android phone of Alphabet Inc!!!!',
+	imagePath: 'http://www.gadgetninja.in/wp-content/uploads/2015/09/letv.jpg',
+	title: 'One +3',
+	description: 'Amazing gazetted smart phone!!!!',
+	price: 19000}),
+	new Product({
+	imagePath: 'https://gigaom.com/wp-content/uploads/sites/1/2013/10/lg-g-flex-curved-640x426.jpg',
+	title: 'Another one from LG',
+	description: 'LG smart phone',
+	price: 10000}),
+	new Product({
+	imagePath: 'http://cdn.xyloor.com/id/Micromax-Yu-Yureka-4G.jpg',
+	title: 'Micromax YU-Yureka ',
+	description: 'Made in India For Indians',
 	price: 12000}),
 	new Product({
-	imagePath: 'http://gadgetfix.com/blogadmin/wp-content/uploads/2016/07/Orange-County-CA-Mobile-Parts-Supplier-Smartphone-Pros-Cons.png',
-	title: 'Orange Country CA Mobile',
-	description: 'CA Mobile by Orange',
-	price: 112000}),
+	imagePath: 'http://img.techentice.com/img/priv.jpg',
+	title: 'Black Berry Mobile',
+	description: 'Latest smartphone from Blackberry Inc',
+	price: 39000}),
 	new Product({
-	imagePath: 'https://static.acer.com/up/Resource/Acer/Smartphones/Liquid%20Z630S/Images/20151001/Acer-smartphone-Liquid-Z630S-Black-gold-preview.png',
-	title: 'HTC Mobile',
-	description: 'Make in India mobile',
-	price: 1200}),
+	imagePath: 'http://www.sagmart.com/other_images/Samsung-Galaxy-S5.jpg',
+	title: 'SamSung Galaxy S5',
+	description: 'Samsung Mobile',
+	price: 48000}),
 	new Product({
-	imagePath: 'http://article.images.consumerreports.org/prod/content/dam/cro/news_articles/Electronics/CR-ELectronics-Smartphone-Battery-Life-Motorola-Droid-Turbo-BN-II-2-16',
-	title: 'Moto',
-	description: 'Moto by Lenovo and developed by Google',
-	price: 1800}),
-	new Product({
-	imagePath: 'http://cdn01.androidauthority.net/wp-content/uploads/2014/10/Honor-6-white-710x310.png',
-	title: 'HTC',
-	description: 'HTC Latest',
-	price: 1800}),
-	new Product({
-	imagePath: 'http://assets.reviews.com/uploads/2015/09/18190915/Smartphone1.jpg',
-	title: 'Apple iPhone 7',
-	description: 'Latest from the Apple Inc',
-	price: 72000}),
+	imagePath: 'http://cdn.gsmarena.com/imgroot/news/15/10/letv-le-1s-official/inline/-728/gsmarena_005.jpg',
+	title: 'LeEco 1s',
+	description: 'LeEco Made in China cool smartphone',
+	price: 8500}),
 ];
 
 
